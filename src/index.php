29623443f751696388e516344b595d01ace3af8f
Cor Haanstra <cor.Haanstra@drukwerkdeal.com>
2013-08-09 13:49:47 -0400

    Testing Github webhooks

--- src/index.php
+++ src/index.php
@@ -1,14 +1,14 @@
 <?php
-	# Aangezien dit verboden is ben ik benieuwd wat hij dan nu gaat doen
-	echo "Hello wORLd";
-	echo "<br>";
-	echo "Is dit niet geweldig";
-	echo "dan gaan we nu een fout toevoegen";
+echo "Hello wORLd";
+echo "<br>";
+echo "Is dit niet geweldig";
+echo "dan gaan we nu een fout toevoegen";
 
 /*
 *	Nog een extra comment voor de volgende functie
 */
+
 function appels($text){
-	echo $test;
-	return $text;
+echo $text;
+return $text;
 }
